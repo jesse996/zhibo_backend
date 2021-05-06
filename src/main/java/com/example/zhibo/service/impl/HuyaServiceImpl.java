76e61538c6eabc68b93e1880621dccba83ab83ad
jesse <szx961102@gmail.com>
2021-05-06 18:00:52 +0800

    add huya live func

--- src/main/java/com/example/zhibo/service/impl/HuyaServiceImpl.java
+++ src/main/java/com/example/zhibo/service/impl/HuyaServiceImpl.java
@@ -3,6 +3,8 @@ package com.example.zhibo.service.impl;
 import cn.hutool.core.codec.Base64;
 import cn.hutool.core.util.ReUtil;
 import cn.hutool.core.util.StrUtil;
+import cn.hutool.core.util.URLUtil;
+import cn.hutool.crypto.digest.MD5;
 import cn.hutool.json.JSONArray;
 import cn.hutool.json.JSONObject;
 import cn.hutool.json.JSONUtil;
@@ -16,14 +18,45 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.servlet.support.JstlUtils;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Service
 public class HuyaServiceImpl implements HuyaService {
     @Autowired
     OkHttpClient client;
+
+    private String live(String e) {
+        String[] split = e.split("\\?");
+        String i = split[0];
+        String b = split[1];
+        String[] r = i.split("/");
+        var s = ReUtil.replaceAll(r[r.length - 1], "\\.(flv|m3u8)", "");
+        var c = b.split("&", 4);
+        c = Arrays.stream(c).filter(_i -> !_i.equals("")).toArray(String[]::new);
+        var n = new HashMap<String, String>();
+        for (var _i : c) {
+            n.put(_i.split("=")[0], _i.split("=")[1]);
+        }
+        var fm = URLUtil.decode(n.get("fm"));
+        var u = new String(Base64.decode(fm));
+        var p = u.split("_")[0];
+        Instant now = Instant.now();
+        long second = now.getEpochSecond();
+        long nano = now.getNano();
+        long f = (second * 1000000000L + nano) / 100;
+        var ll = n.get("wsTime");
+        var t = "0";
+        var h = Arrays.stream((new String[]{p, t, s, String.valueOf(f), ll})).collect(Collectors.joining("_"));
+        var m = new String(MD5.create().digestHex(h));
+        var y = c[c.length - 1];
+        return StrUtil.format("{}?wsSecret={}&wsTime={}&u={}&seqid={}&{}", i, m, ll, t, f, y);
+    }
 
     @Override
     public String getPlayUrl(String rid) {
@@ -46,11 +79,14 @@ public class HuyaServiceImpl implements HuyaService {
             String res = "";
             if (!StrUtil.isBlank(liveLineUrl)) {
                 if (StrUtil.contains(liveLineUrl, "replay")) {
+                    //重播
                     res = "https:" + liveLineUrl;
                 } else {
-
+                    //直播
+                    res = live(liveLineUrl);
                 }
             }
+            return res;
         } catch (Exception e) {
             e.printStackTrace();
         }
